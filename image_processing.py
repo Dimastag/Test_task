697d936c45ce427f92cf00d986e16ef88f4e4089
Dimastag <dmstakors@gmail.com>
2024-05-16 21:47:51 +0300

    Draft

---
+++ image_processing.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+
+class Processing:
+
+
+    def __init__(self):
+        pass
+
+
+    def coordinates(self):
+        pass
+
+    def standart_deviation(self):
+        pass
+
+    def dispersion(self):
+        pass
+
+    def data_parser(self):
+        pass
+
+
+if __name__ == "__main__":
+    process = Processing
+    process.coordinates()
