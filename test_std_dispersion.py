d995ec77d835de8c525f2cc838200da0411b5161
Dimastag <dmstakors@gmail.com>
2024-06-25 19:13:37 +0300

    Added requirements file and several fixtures in test file

--- test_std_dispersion.py
+++ test_std_dispersion.py
@@ -5,13 +5,32 @@ import math
 from image_processing import Processing
 
 
-def test_dispersion():
-
-    processing = Processing()
-
-    std_value = processing.std
-    dispersion_value = processing.disp
+@pytest.fixture
+def processing():
+    return Processing()
 
 
-    assert dispersion_value == std_value ** 2
+@pytest.fixture
+def std(processing):
+    return processing.std
 
+
+@pytest.fixture
+def dispersion(processing):
+    return processing.disp
+
+
+def test_dispersion(std, dispersion):
+
+    std_value = std
+    dispersion_value = dispersion
+
+    assert dispersion_value == std_value ** 2, " Дисперсия олжна быть равна квадрату среднеквадратичного отклонения "
+
+
+def test_std(std, dispersion):
+
+    std_value = std
+    dispersion_value = dispersion
+
+    assert std_value == math.sqrt(dispersion_value), ""
